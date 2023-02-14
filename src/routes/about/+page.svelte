7d67fd82d7470000d8d66be7b7889e75601042fc
MrTango <md@derico.de>
2023-02-14 11:55:51 +0200

    improve about and help page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,14 @@
 <article>
   <h1>About this service</h1>
 
-  We are aggreating all Plone related packages from PyPi and combining it with
+  <p>We are aggreating all Plone related packages from PyPi and enhancing it with
   statistical data from GitHub. This allows people to find Plone add-on's easier
   and decide what to use and in which versions. The data from PyPi is updated
-  once an hour and the data from GitHub every night.
+  once an hour and the data from GitHub every night.</p>
+
+  <p>If you think we can improve something or found something not working, please let us know by opening an issue here:</p>
+
+  <a href="https://github.com/collective/pyf-gui/issues">https://github.com/collective/pyf-gui/issues</a>
 
   <!-- <p>This service brought to you by <a href="https://derico.de">Derico</a></p> -->
 </article>
--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -5,31 +5,40 @@
   To make your Plone package look good, you can make sure that you defined the following PyPi classifieres.
 
   For supported Plone versions:
-  <pre>
-    "Framework :: Plone",
-    "Framework :: Plone :: Addon",
-    "Framework :: Plone :: 5.2",
-    "Framework :: Plone :: 6.0",
-  </pre>
+
+<pre>
+"Framework :: Plone",
+"Framework :: Plone :: 5.2",
+"Framework :: Plone :: 6.0",</pre>
 
   For supported Python versions:
-  <pre>
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-  </pre>
+
+<pre>"Programming Language :: Python",
+"Programming Language :: Python :: 3.8",
+"Programming Language :: Python :: 3.9",
+"Programming Language :: Python :: 3.10",
+"Programming Language :: Python :: 3.11",</pre>
+
+  set the right type of your package, choose one of this:
+
+<pre>"Framework :: Plone :: Addon",
+"Framework :: Plone :: Theme",
+"Framework :: Plone :: Distribution",
+"Framework :: Plone :: Core",</pre>
+
+  Please do not put "Distribution" or "Core" for normal add-on's, as it make filtering them later harder.
+  Only use "Core" for Plone core packages, which are comming with Plone be default.
+
 
   Also make sure that you have well defined urls to GitHub, docs, homepage aso:
-  <pre>
-    project_urls="&#123;
-      "PyPI": "https://pypi.python.org/pypi/plone.pdfexport",
-      "Source": "https://github.com/plone/plone.pdfexport",
-      "Tracker": "https://github.com/plone/plone.pdfexport/issues",
-      'Documentation': 'https://plone.pdfexport.readthedocs.io/en/latest/',
-    &#125;",
-  </pre>
+
+<pre>
+project_urls="&#123;
+    "PyPI": "https://pypi.python.org/pypi/plone.pdfexport",
+    "Source": "https://github.com/plone/plone.pdfexport",
+    "Tracker": "https://github.com/plone/plone.pdfexport/issues",
+    'Documentation': 'https://plone.pdfexport.readthedocs.io/en/latest/',
+  &#125;",</pre>
 
   <!-- <p>This service brought to you by <a href="https://derico.de">Derico</a></p> -->
 </article>
@@ -39,5 +48,10 @@
     grid-area: content;
     padding: 1em;
     min-height: 50vh;
+  }
+  pre{
+    margin: 1em 0;
+    padding: 0.5em;
+    background-color: wheat;
   }
 </style>
