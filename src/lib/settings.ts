5e443a68529836076912b1a28fb5c64097287b9c
MrTango <md@derico.de>
2026-01-23 12:24:16 +0200

    Update default filters to include Plone 6.1 and Addon type
    Enable Addon filter and add Plone 6.1 to default selected versions
    for a more current default search experience.

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,12 +1,13 @@
 // export let api_url:string;
 
 export const default_plone_versions = [
+  "Plone 6.1",
   "Plone 6.0",
   "Plone 5.2"
 ];
 
 export const default_package_types = [
-  // "Framework :: Plone :: Addon",
+  "Framework :: Plone :: Addon",
   // "Framework :: Plone :: Theme",
   // "Framework :: Plone :: Distribution"
 ];
